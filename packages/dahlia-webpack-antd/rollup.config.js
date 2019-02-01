7a015449deb839d938c4eda40e68cacc84d1b339
forsigner <forsigner@gmail.com>
2019-02-01 10:20:35 +0800

    feat: do not use named exprot

--- packages/dahlia-webpack-antd/rollup.config.js
+++ packages/dahlia-webpack-antd/rollup.config.js
@@ -9,13 +9,11 @@ export default {
     {
       file: pkg.main,
       format: 'cjs',
-      exports: 'named',
       sourcemap: true,
     },
     {
       file: pkg.module,
       format: 'es',
-      exports: 'named',
       sourcemap: true,
     },
   ],

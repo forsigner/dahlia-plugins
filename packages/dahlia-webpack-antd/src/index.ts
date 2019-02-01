437dd28270ed059a33be98491d9ef9c1928d497a
forsigner <forsigner@gmail.com>
2019-02-01 15:07:01 +0800

    refactor: use customize-cra

--- packages/dahlia-webpack-antd/src/index.ts
+++ packages/dahlia-webpack-antd/src/index.ts
@@ -1,30 +1,4 @@
-const getBabelLoader = config => {
-  const babelLoaderFilter = rule =>
-    rule.loader &&
-    rule.loader.includes('babel') &&
-    rule.options &&
-    rule.options.plugins
-
-  // First, try to find the babel loader inside the oneOf array.
-  // This is where we can find it when working with react-scripts@2.0.3.
-  let loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf))
-    .oneOf
-
-  let babelLoader = loaders.find(babelLoaderFilter)
-
-  // If the loader was not found, try to find it inside of the "use" array, within the rules.
-  // This should work when dealing with react-scripts@2.0.0.next.* versions.
-  if (!babelLoader) {
-    loaders = loaders.reduce((ldrs, rule) => ldrs.concat(rule.use || []), [])
-    babelLoader = loaders.find(babelLoaderFilter)
-  }
-  return babelLoader
-}
-
-const addBabelPlugin = plugin => config => {
-  getBabelLoader(config).options.plugins.push(plugin)
-  return config
-}
+import { addBabelPlugin } from 'customize-cra'
 
 const antd = options => config => {
   const { style = 'css' } = options
--- packages/dahlia-webpack-styled-components/src/index.ts
+++ packages/dahlia-webpack-styled-components/src/index.ts
@@ -1,32 +1,9 @@
-const getBabelLoader = config => {
-  const babelLoaderFilter = rule =>
-    rule.loader &&
-    rule.loader.includes('babel') &&
-    rule.options &&
-    rule.options.plugins
+import { addBabelPlugin } from 'customize-cra'
 
-  // First, try to find the babel loader inside the oneOf array.
-  // This is where we can find it when working with react-scripts@2.0.3.
-  let loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf))
-    .oneOf
-
-  let babelLoader = loaders.find(babelLoaderFilter)
-
-  // If the loader was not found, try to find it inside of the "use" array, within the rules.
-  // This should work when dealing with react-scripts@2.0.0.next.* versions.
-  if (!babelLoader) {
-    loaders = loaders.reduce((ldrs, rule) => ldrs.concat(rule.use || []), [])
-    babelLoader = loaders.find(babelLoaderFilter)
+const styledComponents = () => (config, env) => {
+  if (env !== 'development') {
+    return config
   }
-  return babelLoader
-}
-
-const addBabelPlugin = plugin => config => {
-  getBabelLoader(config).options.plugins.push(plugin)
-  return config
-}
-
-const styledComponents = () => config => {
   const newConfig = addBabelPlugin('babel-plugin-styled-components')(config)
   return newConfig
 }
